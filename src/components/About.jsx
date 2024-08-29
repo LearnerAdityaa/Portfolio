b52766396b8373a105fbfc52fa4d126cb1dec7a9
LearnerAdityaa <revengeforcoc@gmail.com>
2024-08-29 23:04:46 +0545

    all fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import AboutImage from "../assets/About.jpg";
+import Hero1Image from "../assets/Hero1.jpg";
 
 function About() {
   return (
@@ -9,7 +9,7 @@ function About() {
         <h2 className="text-4xl font-bold text-center mb-12">About Me</h2>
         <div className="flex flex-col md:flex-row items-center md:space-x-12">
           <img
-            src={AboutImage}
+            src={Hero1Image}
             alt="about-image"
             className="w-72 h-80 rounded object-cover mb-8 md:mb-0"
             style={{ borderRadius: "40px" }}
