e7aa240f9b87ee1e7861b85ee1f46074a5e013b8
LearnerAdityaa <revengeforcoc@gmail.com>
2024-08-29 22:57:56 +0545

    Normalize line endings

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import AboutImage from "../assets/About.jpg"; 
+import AboutImage from "../assets/About.jpg";
 
 function About() {
   return (
     <div className="bg-black text-white py-20" id="about">
       <div className="container mx-auto px-8 md:px-16 lg:px-24">
         {" "}
-       
         <h2 className="text-4xl font-bold text-center mb-12">About Me</h2>
         <div className="flex flex-col md:flex-row items-center md:space-x-12">
           <img
-            src={AboutImage} 
+            src={AboutImage}
             alt="about-image"
             className="w-72 h-80 rounded object-cover mb-8 md:mb-0"
             style={{ borderRadius: "40px" }}
           />
+
           <div className="flex-1">
             <p className="text-lg mb-8">
               I am a passionate MERN-stack developer with a focus on building
