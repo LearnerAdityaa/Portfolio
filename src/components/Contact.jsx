c6a770fdcf4e6ca4ab76c20804d112892f1ff308
LearnerAdityaa <revengeforcoc@gmail.com>
2024-08-29 23:28:02 +0545

    button fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -97,7 +97,7 @@ function Conatct() {
               <input
                 type="submit"
                 value="Send"
-                className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
+                className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
               />
             </form>
           </div>
